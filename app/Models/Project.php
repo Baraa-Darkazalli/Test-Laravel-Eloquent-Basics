2839a6bd03b98e03dcff8a13a3d936c2a8512e19
Baraa Darkazalli <aboomarbaraa@gmail.com>
2024-10-21 16:48:27 +0300

    Elquent Tasks

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,4 +9,19 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Project extends Model
 {
     use HasFactory, SoftDeletes;
+
+    protected $guarded = [];
+
+    public static function boot()
+{
+    parent::boot();
+
+    self::creating(function ($model) {
+        Stat::all()->each(function ($stat) {
+            return $stat->update([
+                'projects_count'    => $stat->projects_count + 1
+            ]);
+        });
+    });
+}
 }
